eb4986889c139c136e2afd3b26da10ce1821352f
Sebastian Möser <sebastian@moeser.se>
2022-02-16 21:58:18 +0100

    Add some comments for clarity

--- src/main/java/se/moeser/javacleanscaffold/api/filters/JwtRequestFilter.java
+++ src/main/java/se/moeser/javacleanscaffold/api/filters/JwtRequestFilter.java
@@ -3,15 +3,12 @@ package se.moeser.javacleanscaffold.api.filters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import se.moeser.javacleanscaffold.api.auth.ApiUserDetailsService;
 import se.moeser.javacleanscaffold.api.auth.ApiUserPrincipal;
 import se.moeser.javacleanscaffold.api.auth.JwtTokenUtil;
-import se.moeser.javacleanscaffold.api.dto.AuthenticationResponse;
 import se.moeser.javacleanscaffold.application.usecase.user.authenticate.AuthenticateUserResponse;
 import se.moeser.javacleanscaffold.domain.entity.User;
 
@@ -37,6 +34,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         long userId = 0;
         String jwt = null;
 
+        // Find the user id in the token payload
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
             userId = jwtTokenUtil.extractUserId(jwt);
@@ -48,6 +46,9 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             User user = new User();
             user.setId(userId);
 
+            // Since the token is signed we can trust the user id from the payload
+            // Wrap the user in an AuthenticateUserResponse without checking with the database first
+            // This saves us one lookup for each authenticated request.
             ApiUserPrincipal userDetails = new ApiUserPrincipal(new AuthenticateUserResponse(user));
 
             if (jwtTokenUtil.validateToken(jwt, userDetails)) {
